f0b0c83cfc89fd45b88999944e1aca0be26554ae
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2023-02-23 11:09:34 -0300

    .

--- AULA6_exercicio1/app.js
+++ AULA6_exercicio1/app.js
@@ -1,56 +1,95 @@
-/*****************************************************************
- * Objetivo: Usar calcálos matemáticos (soma, subtração, divisão, multiplicação).
- * Data: 03/02//2023
- * Autor: Ingryd
- * Versão: 1.0
- ******************************************************************/
-var oi = require('./modulo/media.js')
+var calculoMedia = require('./modulo/media.js')
+    //import da biblioteca readline
 var readline = require('readline');
+const { exame } = require('./modulo/media.js');
 
+
+//Cria o objeto para ser especialista em entrada de dados pelo teclado
 var entradaDados = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//Função de callback para entrar com o nome do aluno
+entradaDados.question('Digite o nome do aluno: ', function(nome) {
+    let nomeAluno = nome;
 
-entradaDados.question('Nome do aluno: ', function(nomeAluno) {
+    entradaDados.question('Qual o sexo do aluno (M ou F)? ', function(sexoAluno) {
+        let sexoAl = sexoAluno.toUpperCase()
 
-    entradaDados.question('Sexo do aluno [FEMININO | MASCULINO]: ', function(sexoAluno) {
-        let sexoDoAluno = sexoAluno;
-        if (sexoDoAluno == 'feminino') {
-            console.log('oi');
-        } else if (sexoDoAluno == 'masculino') {
-            console.log('tchau');
-        }
+        entradaDados.question('Qual o nome do professor: ', function(nomeProfessor) {
+            let nomeProf = nomeProfessor;
 
-        entradaDados.question('Nome do professor: ', function(nomeProfessor) {
+            entradaDados.question('Qual o sexo do professor (M ou F)?', function(sexoProfessor) {
+                let sexoProf = sexoProfessor.toUpperCase()
 
-            entradaDados.question('Nome do curso: ', function(NomeCurso) {
+                entradaDados.question('Qual o nome do curso: ', function(curso) {
+                    let nomeCurso = curso
 
-                entradaDados.question('Nome da disciplina: ', function(NomeDisciplina) {
+                    entradaDados.question('Qual o nome da disciplina: ', function(disciplina) {
+                        let nomeDisciplina = disciplina
 
-                    entradaDados.question('Digite a nota 1: ', function(c) {
+                        entradaDados.question('Digite a nota 1: ', function(nota1) {
+                            let valor1 = nota1;
 
-                        entradaDados.question('Digite a nota 2: ', function(numero2) {
 
-                            entradaDados.question('Digite a nota 3: ', function(numero3) {
+                            entradaDados.question('Digite a nota 2: ', function(nota2) {
+                                let valor2 = nota2;
 
-                                entradaDados.question('Digite a nota 4: ', function(numero4) {
 
-                                });
+                                entradaDados.question('Digite a nota 3: ', function(nota3) {
+                                    let valor3 = nota3;
 
-                            });
 
+                                    entradaDados.question('Digite a nota 4: ', function(nota4) {
+                                        let valor4 = nota4;
+                                        let media;
+
+                                        media = calculoMedia.calculoMedia(valor1, valor2, valor3, valor4)
+
+                                        if (media >= 50 && media <= 69) {
+                                            console.log('O aluno precisa fazer o exame');
+                                            entradaDados.question('Digite a nota do exame: \n', function(notaExame) {
+
+                                                let resultado = calculoMedia.exame(media, notaExame);
+
+                                                if (resultado <= 59) {
+                                                    console.log(`${calculoMedia.sexoAluno(sexoAl)} ${nomeAluno} foi REPROVADO na disciplina ${nomeDisciplina}`);
+                                                    console.log(`Curso: ${nomeCurso}`);
+                                                    console.log(`${calculoMedia.sexoProfessor(sexoProf)} : ${nomeProfessor}`);
+                                                    console.log(`Notas do Aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaExame}`);
+                                                    console.log(`Média Final: ${media}`);
+                                                    console.log(`Média do Exame: ${resultado}`);
+                                                } else if (resultado >= 60) {
+                                                    console.log(`${calculoMedia.sexoAluno(sexoAl)} ${nomeAluno} foi APROVADO na disciplina ${nomeDisciplina}`);
+                                                    console.log(`Curso: ${nomeCurso}`);
+                                                    console.log(`${calculoMedia.sexoProfessor(sexoProf)} : ${nomeProfessor}`);
+                                                    console.log(`Notas do Aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}, ${notaExame}`);
+                                                    console.log(`Média Final: ${media}`);
+                                                    console.log(`Média do Exame: ${resultado}`);
+                                                }
+                                            })
+
+                                        } else if (media < 50) {
+                                            console.log(`${calculoMedia.sexoAluno(sexoAl)} ${nomeAluno} foi REPROVADO na disciplina ${nomeDisciplina}`);
+                                            console.log(`Curso: ${nomeCurso}`);
+                                            console.log(`${calculoMedia.sexoProfessor(sexoProf)} : ${nomeProfessor}`);
+                                            console.log(`Notas do Aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}`);
+                                            console.log(`Média Final: ${media}`);
+                                        } else if (media > 70) {
+                                            console.log(`${calculoMedia.sexoAluno(sexoAl)} ${nomeAluno} foi APROVADO na disciplina ${nomeDisciplina}`);
+                                            console.log(`Curso: ${nomeCurso}`);
+                                            console.log(`${calculoMedia.sexoProfessor(sexoProf)} : ${nomeProfessor}`);
+                                            console.log(`Notas do Aluno: ${valor1}, ${valor2}, ${valor3}, ${valor4}`);
+                                            console.log(`Média Final: ${media}`);
+                                        }
+                                    })
+                                })
+                            })
                         });
-
                     });
-
                 });
-
             });
-
-        });
-
-    });
-
-});
+        })
+    })
+})
--- AULA6_exercicio1/modulo/media.js
+++ AULA6_exercicio1/modulo/media.js
@@ -1,69 +1,84 @@
-/****************************************************
- * Objetivo: arquivo de funções para realizar cálculos medias
- * Data: 10/02/2023
- * Autor: Ingryd 
- * Versão: 1.0
- ****************************************************/
-const media = function(nomeAluno, sexoAluno, nomeProfessor, sexoProfessor, nomeCurso, nomeDisciplina, numero1, numero2, numero3, numero4) {
-    let nomeAluno = nomeAluno;
-    let sexoAluno = sexoAluno.toUpperCase();
-    let nomeProfessor = nomeProfessor;
-    let sexoProfessor = sexoProfessor.toUpperCase();
-    let nomeCurso = nomeCurso;
-    let nomeDisciplina = nomeDisciplina;
-    let nota1 = Number(numero1);
-    let nota2 = Number(numero2);
-    let nota3 = Number(numero3);
-    let nota4 = Number(numero4);
-    let relatorioFem;
-    let relatorioMasc;
+const calculoMedia = function(valor1, valor2, valor3, valor4) {
 
-    let media = 0;
-    let exameRecuperacao;
-    let resultado;
+    media = (Number(valor1) + Number(valor2) + Number(valor3) + Number(valor4)) / 4;
 
-    // TRATATIVA DE ERRO
-    if (numero1 == '' || numero2 == '' || numero3 == '' || numero4 == '') {
-        console.log('Erro: é necessário digitar algum valor nas notas.')
+    //Validação para entrada vazia 
+    if (valor1 == '' || valor2 == '' || valor3 == '' || valor4 == '') {
+        console.log('ERRO: há espaços sem valor atribuidos.')
 
-    } else if (isNaN(numero1) || isNaN(numero2) || isNaN(numero3) || isNaN(numero4)) {
-        console.log('É necessário que todos os dados digitados sejam números.')
+        //Validação para entrada de texto(inválida)    
+    } else if (isNaN(valor1) || isNaN(valor2) || isNaN(valor3) || isNaN(valor4)) {
 
-    } else if (numero1 > 100 || numero1 < 0 || numero2 > 100 || numero2 < 0 ||
-        numero3 > 100 || numero3 < 0 || numero4 > 100 || numero4 < 0 || exameRecuperacao > 100 || exameRecuperacao < 0) {
-        console.log('Erro: digite números entre 0 e 100.');
+        console.log('ERRO: Você não digitou um número')
+            //Validação para entrada de número menor que 0(inválida)
+    } else if (valor1 < 0 || valor2 < 0 || valor3 < 0 || valor4 < 0) {
+
+        console.log('ERRO: Nota menor que 0 não é válida')
+            //Validação para entrada de número maior que 10(inválida)
+    } else if (valor1 > 100 || valor2 > 100 || valor3 > 100 || valor4 > 100) {
+
+        console.log('ERRO: Nota maior que 100 não é válida')
 
     } else {
-        media = (parseFloat(numero1) + parseFloat(numero2) + parseFloat(numero3) + parseFloat(numero4)) / 4;
-
-        if (media >= 70) {
-            console.log('Status do aluno: APROVADO');
-
+        console.log('Sua média foi:', media.toFixed(1));
+        if (media < 50) {
+            return media
         } else if (media >= 50 && media <= 69) {
-            console.log('Você terá que fazer o exame de recuperação!')
-
-        } else {
-            console.log('Status do aluno: REPROVADO \n');
-        }
-
-        function resultado() {
-            if (media >= 70) {
-                console.log('Status do aluno: APROVADO');
-            } else if (media >= 50 && media <= 69) {
-                console.log('Você terá que fazer o exame de recuperação!');
-            } else {
-                console.log('Status do aluno: REPROVADO \n');
-            }
-        }
-    }
-
-    function relatorio(nomeAluno, sexoAluno, nomeProfessor, sexoProfessor, NomeCurso, NomeDisciplina, numero1, numero2, numero3, numero4) {
-        if (sexoAluno == 'feminino' && sexoProfessor == 'feminino') {
-            console.log('A aluna ' + nomeAluno + 'foi ' + resultado + ' na disciplina. \n' + nomeDisciplina + 'Curso: \n' + nomeCurso + 'Professor: \n' + nomeProfessor + 'Notas da aluna:' + nota1 + nota2 + nota3 + nota4 + 'Média Final: ' + media)
+            exame()
+            return media
+        } else if (media >= 70) {
+            return media
         }
     }
 }
 
+const exame = function(calculoMedia, notaExame) {
+
+    let exame = (parseFloat(calculoMedia) + parseFloat(notaExame)) / 2;
+
+    if (exame >= 60) {
+        //console.log('Com a nota do exame - APROVADO');
+        return exame
+    } else if (exame <= 59) {
+        //console.log('Com a nota do exame - REPROVADO');
+        return exame
+    }
+}
+
+
+const sexoAluno = function(sexoAl) {
+    let generoAl = sexoAl;
+
+    switch (generoAl) {
+        case 'M':
+            generoAl = 'O aluno'
+            break;
+
+        case 'F':
+            generoAl = 'A aluna'
+            break;
+    }
+    return generoAl;
+}
+
+const sexoProfessor = function(sexoProf) {
+    let generoProfessor = sexoProf;
+
+    switch (generoProfessor) {
+        case 'MASCULINO':
+            generoProfessor = 'Professor'
+            break;
+
+        case 'FEMININO':
+            generoProfessor = 'Professora'
+            break;
+    }
+    return generoProfessor;
+}
+
 module.exports = {
-    media
+    calculoMedia,
+    exame,
+    sexoAluno,
+    sexoProfessor
 }
