85142c484487a27573495801ed7e1e82859cada4
INGRYD SHIRLLEY DA SILVA RIBEIRO <22282092@CFP127ALUNO.LOCAL>
2023-03-13 11:25:45 -0300

    funcos 1,2 e 3 prontas utilizando o postman

--- aula-08-atividade/app.js
+++ aula-08-atividade/app.js
@@ -5,45 +5,149 @@
  * Versão: 1.0
  *************************************************************************/
 
-/******************************************************
- * EXPRESS - "npm install express --save"
- * CORS - "npm install cors --save"
- * BODY-PARSER - "npm install body-parser --save"
- ******************************************************/
+/*************EXPLICAÇÃO*************
+ * Express - dependencia do Node q permite a integração entre o protocolo 
+HTTP com o código: npm install express --save
 
-//import das dependencias para criar a api:
-//responsavel pelas requisiçoes
+ * Cors - gerenciador de permissões para o protocolo HTTP: npm install 
+cors --save
+
+ * Body-parser - dependencia que permite manipular dados enviados pelo 
+body da requisição: npm install body-parser --save
+ ************************************/
+
+/* Import das dependencias para criar a API */
+//responsavel pelas requisições
 const express = require('express');
-//responsavel pelas permiçoes das requisiçoes
+//resposável pelas permissões das requisições
 const cors = require('cors');
-//responsavel pela manipulaçao do body da requisiçao
+//responsável pela manipulação do body da requisição
 const bodyParser = require('body-parser');
+//import do arquivo de funcoes
+const estadosCidades = require('./modulo/estados_cidades.js')
 
-//cria um objeto com a informaçoes da classe express
+
+
+
+
+//cria um objeto com as informações da classe express
 const app = express();
+
+//define as permissões no header da API
 app.use((request, response, next) => {
-    response.header('Access-Control-Allow-Origin', '*');
-    //permite gerenciar a origem das requisições da api
-    //'*' - (publica) / '200.100.0.45' - IP especifico (privada)
+    //Permite gerenciar a origem das requisições da API
+    //'*' - de qualquer lugar (PUBLICA)
+    //'200.100.0.45' - de um IP especifico, responderá somente aquela maquina (PRIVADA)
+    response.header('Access-Control-Allow-Origin', '*')
 
-    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    //diz quais metodos eu vou liberar pra essa api
+    //verbos(methods) que poderão ser usados como requisição
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 
-    app.use(cors());
+    //ativa no cors das requisições as permissões estabelecidas
+    app.use((cors));
     next();
 });
-//endPoints
 
-//endPoints para listar os estados
+
+//endPoints, cors, async(requisição asíncrona)
+
+/* *********FUNÇÃO 1********* */
+//endPoint para Listar os Estados
 app.get('/estados', cors(), async function(request, response, next) {
 
-    const estadosCidades = require('./modulo/estados_cidades.js');
+    //chama a funcao que retorna os estados
     let listaDeEstados = estadosCidades.getListaDeEstados();
-    response.json(listaDeEstados);
-    response.status(200);
+    //tratamento p validar se a funçao validou o processamento
+    if (listaDeEstados) {
+        //retorna o json e o status code
+        response.json(listaDeEstados);
+        response.status(200);
+    } else {
+        response.status(500);
+    }
 });
 
-//permite carregar os endpoints criados e aguardar as requisicoes pelo protocolo http na porta 8080
+/* *********FUNÇÃO 2********* */
+//endPoint para Listar as caracteristicas do estado pela sigla
+app.get('/estado/sigla/:uf', cors(), async function(request, response, next) {
+    //:uf é uma variavel q sera utilizada p passagem de valores na url da requisiçao
+
+    let siglaEstado = request.params.uf;
+    let statusCode;
+    let dadosEstado = {};
+
+    // recebe o valor da variavel uf que sera encaminhada na url da requisição
+    if (siglaEstado == '' || siglaEstado == undefined || siglaEstado.length != 2 || !isNaN(siglaEstado)) {
+        dadosEstado.message = "nao é possivel processar a requisição pois a sigla do estado nao foi informada ou nao atende a quantidade de caracteres (2 digitos)";
+        statusCode = 400;
+    } else {
+        let estado = estadosCidades.getDadosEstados(siglaEstado);
+        if (estado) {
+            dadosEstado = estado;
+            statusCode = 200;
+        } else {
+            statusCode = 404;
+        }
+    }
+    response.status(statusCode);
+    response.json(dadosEstado)
+});
+
+/* *********FUNÇÃO 3********* */
+//endPoint para pegar a capital do estado pela sigla
+app.get('/capitalEstado/sigla/:uf', cors(), async function(request, response, next) {
+    let siglaEstado = request.params.uf;
+    let statusCode;
+    let dadosCapitalEstado = {};
+
+    if (siglaEstado == '' || siglaEstado == undefined || siglaEstado.length != 2 || !isNaN(siglaEstado)) {
+        dadosCapitalEstado.message = "Não é possível processar a requisição, pois a sigla do estado não foi informada ou não atende a quantidade de caracteres (2 digitos)";
+        statusCode = 400;
+    } else {
+        //chama a função que filtra o estado pela sigla 
+        let capital = estadosCidades.getCapitalEstado(siglaEstado)
+
+        //valida se houve retorno válido da função
+        if (capital) {
+            statusCode = 200; //Estado encontrado
+            dadosCapitalEstado = capital;
+        } else {
+            statusCode = 404; //Estado não encontrado
+        }
+    }
+    response.status(statusCode);
+    response.json(dadosCapitalEstado)
+});
+
+/* *********FUNÇÃO 4********* */
+//endPoint para pegar a capital do estado pela sigla
+app.get('/estadosRegiao/regiaoEstado/:jRegiao', cors(), async function(request, response, next) {
+    let nomeRegiao = request.params.jRegiao;
+    let statusCode;
+    let dadosEstadoRegiao = {};
+
+    if (nomeRegiao == '' || nomeRegiao == undefined || !isNaN(nomeRegiao)) {
+        dadosEstadoRegiao.message = "Não é possível processar a requisição, pois a sigla do estado não foi informada ou não atende a quantidade de caracteres (2 digitos)";
+        statusCode = 400;
+    } else {
+        //chama a função que filtra o estado pela sigla 
+        let jRegiao = estadosCidades.getEstadosRegiao(nomeRegiao)
+
+        //valida se houve retorno válido da função
+        if (jRegiao) {
+            statusCode = 200; //Estado encontrado
+            dadosEstadoRegiao = jRegiao;
+        } else {
+            statusCode = 404; //Estado não encontrado
+        }
+    }
+    response.status(statusCode);
+    response.json(dadosEstadoRegiao)
+});
+
+
+//permite carregar os endPoint criados e aguardar as requisições
+//pelo protocolo HTTP na porta 8080
 app.listen(8080, function() {
-    console.log("servidor aguardando requisicoes na porta 8080")
-})
+    console.log('Servidor aguardando requisições na porta 8080.')
+});
